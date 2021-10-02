bc602654c6cca70c543261a78fd8ffbc244ae3c5
wagnerlemos94 <wagner.lemos94@hotmail.com>
2021-10-02 04:28:51 -0300

    Projeto Criado

---
+++ src/main/java/com/vidasonora/lemos/LemosApplication.java
@@ -0,0 +1,13 @@
+package com.vidasonora.lemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vidasonora/lemos/LemosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidasonora.lemos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
