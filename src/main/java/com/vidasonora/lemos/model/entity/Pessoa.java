faacd9706b4812a62d6323e0bdee6082e86adc4f
wagnerlemos94 <wagner.lemos94@hotmail.com>
2021-10-02 04:43:43 -0300

    Implementa entitade

---
+++ src/main/java/com/vidasonora/lemos/model/entity/Pessoa.java
@@ -0,0 +1,36 @@
+package com.vidasonora.lemos.model.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter @Setter
+@ToString
+@Entity
+@Table(name = "tb_pessoa")
+public class Pessoa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String cpf;
+	private String rg;
+	private String profissao;
+	
+	@JsonFormat( shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+	private Date nascimento;
+
+}
